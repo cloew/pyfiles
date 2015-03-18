7242cc352fbdcbd429a4802d9969f9495047fcbe
Chris Loew <cloew123@gmail.com>
2015-03-18 15:40:07 -0600

    Added boilerplate Python Package

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pyfiles',
+      version='0.0.1',
+      description="",
+      author='',
+      author_email='',
+      packages=['pyfiles'],
+     )
